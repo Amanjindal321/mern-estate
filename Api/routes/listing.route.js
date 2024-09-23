51d11f6fd4023afdc1ae7691e1940bb0041dfbe7
Aman <jindalaman405@gmail.com>
2024-09-23 23:12:52 +0530

    commit

--- Api/routes/listing.route.js
+++ Api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListing, deleteListing } from '../contrrollers/listing.controller.js';
+import { createListing, deleteListing, updateListing } from '../contrrollers/listing.controller.js';
 
 const router=express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing)
 export default router;
